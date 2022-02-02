bbf5c8bb7bff6db05ce3dba22dc88f4044a1da6b
jprtoledo <juan.prtoledo@gmail.com>
2022-02-01 22:20:57 -0300

    "Add cool face API"

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
+const cool = require('cool-ascii-faces');
 const express = require('express');
-const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
